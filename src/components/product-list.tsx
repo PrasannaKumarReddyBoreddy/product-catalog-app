942bf39c9ff9a38d374d9b8a8ed4bfad9bfe2192
boreddy02 <prasannakumar.reddy@openlane.com>
2023-07-25 19:55:09 +0530

    re-work product-list to support dynamic catagory list

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -5,12 +5,11 @@ import { Container, Row, Col, Dropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import ProductCard from "./product-card";
 
-
 const ProductList: React.FC = () => {
-
   const [products, setProducts] = useState<Product[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
+  const [CatagoryList, setCatagoryList] = useState<Set<string | null>>(new Set());
 
   useEffect(() => {
     getProducts();
@@ -27,6 +26,16 @@ const ProductList: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    setCatagoryList(() => {
+      const updatedCategoryList = new Set<string | null>();
+      products.forEach((product) => {
+        updatedCategoryList.add(product.category || "");
+      });
+      return updatedCategoryList;
+    });
+  }, [products]);
+
   const handleCategorySelected = (category: string | null) => {
     setSelectedCategory(category);
   };
@@ -35,7 +44,6 @@ const ProductList: React.FC = () => {
     ? products?.filter((product) => product.category === selectedCategory)
     : products;
 
-    
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -44,37 +52,51 @@ const ProductList: React.FC = () => {
     return <div>Products not found!</div>;
   }
 
+  const categoryElement = Array.from(CatagoryList).map((category) => (
+    <Dropdown.Item
+      key={category}
+      onClick={() => handleCategorySelected(category)}
+    >
+      {category}
+    </Dropdown.Item>
+  ));
+
   return (
     <Container>
-        <Row>
-            <Col xs={12} className="mb-3 text-center">
-                <Dropdown  style={{ position: 'relative', margin: '20px' }}>
-                    <Dropdown.Toggle variant="primary" id="categoryDropdown">
-                        {selectedCategory ? `Category: ${selectedCategory}`: 'All Categorys'}
-                    </Dropdown.Toggle>
-                    <Dropdown.Menu>
-                        <Dropdown.Item onClick={()=>handleCategorySelected(null)}>All Category's</Dropdown.Item>
-                        <Dropdown.Item onClick={()=>handleCategorySelected('smartphones')}>SmartPhones</Dropdown.Item>
-                        <Dropdown.Item onClick={()=>handleCategorySelected('laptops')}>Laptops</Dropdown.Item>
-                        <Dropdown.Item onClick={()=>handleCategorySelected('fragrances')}>Fragrances</Dropdown.Item>
-                        <Dropdown.Item onClick={()=>handleCategorySelected('skincare')}>Skincare</Dropdown.Item>
-                        <Dropdown.Item onClick={()=>handleCategorySelected('groceries')}>Groceries</Dropdown.Item>
-                        <Dropdown.Item onClick={()=>handleCategorySelected('home-decoration')}>Home Decorations</Dropdown.Item>
-                    </Dropdown.Menu>
-                </Dropdown>
-            </Col>
-            <Col xs={12}>
-                <Row>
-                    {filterProducts?.map((product)=>(
-                        <Col xs={12} md={4} lg={3} key={product.id} className="mb-3">
-                            <Link to={`/product/${product.id}`} style={{textDecoration: 'none'}}>
-                            <ProductCard product={product} onClick={()=>{}}></ProductCard>
-                            </Link>
-                        </Col>
-                    ))}
-                </Row>
-            </Col>
-        </Row>
+      <Row>
+        <Col xs={12} className="mb-3 text-center">
+          <Dropdown style={{ position: "relative", margin: "20px" }}>
+            <Dropdown.Toggle variant="primary" id="categoryDropdown">
+              {selectedCategory
+                ? `Category: ${selectedCategory}`
+                : "All Categorys"}
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item onClick={() => handleCategorySelected("")}>
+                All Category's
+              </Dropdown.Item>
+              {categoryElement}
+            </Dropdown.Menu>
+          </Dropdown>
+        </Col>
+        <Col xs={12}>
+          <Row>
+            {filterProducts?.map((product) => (
+              <Col xs={12} md={4} lg={3} key={product.id} className="mb-3">
+                <Link
+                  to={`/product/${product.id}`}
+                  style={{ textDecoration: "none" }}
+                >
+                  <ProductCard
+                    product={product}
+                    onClick={() => {}}
+                  ></ProductCard>
+                </Link>
+              </Col>
+            ))}
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
